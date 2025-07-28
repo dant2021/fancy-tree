b7d35d7d322c4288b69aaad70b324d3b7173daa5
Antoine <86591160+dant2021@users.noreply.github.com>
2025-07-28 15:24:22 +0200

    support more languages v0 c,c++,csharp,javascript,php,ruby,rust

--- fancy_tree/extractors/__init__.py
+++ fancy_tree/extractors/__init__.py
@@ -5,6 +5,14 @@ from .python import PythonExtractor
 from .typescript import TypeScriptExtractor
 from .java import JavaExtractor
 from .go import GoExtractor
+from .javascript import JavaScriptExtractor
+from .rust import RustExtractor
+from .c import CExtractor
+from .cpp import CppExtractor
+from .csharp import CsharpExtractor
+from .php import PhpExtractor
+from .ruby import RubyExtractor
+
 
 # Register all available extractors
 def initialize_extractors():
@@ -12,7 +20,14 @@ def initialize_extractors():
     register_extractor("python", PythonExtractor())
     register_extractor("typescript", TypeScriptExtractor())
     register_extractor("java", JavaExtractor())
-    register_extractor("go",     GoExtractor())
+    register_extractor("go", GoExtractor())
+    register_extractor("javascript", JavaScriptExtractor())
+    register_extractor("rust", RustExtractor())
+    register_extractor("c", CExtractor())
+    register_extractor("cpp", CppExtractor())
+    register_extractor("csharp", CsharpExtractor())
+    register_extractor("php", PhpExtractor())
+    register_extractor("ruby", RubyExtractor())
 
 # Auto-initialize when module is imported
 initialize_extractors()
